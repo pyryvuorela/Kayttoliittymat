61778182b67aadace85af7596158de69539ccf1c
Susanna <unsbe92@gmail.com>
2016-03-10 15:55:27 +0200

    modified addNewUser
    no buttons yet, scrolling view still on progress

--- Kayttoliittymat/app/src/main/java/com/example/pyry/kayttoliittymat/addUserNew.java
+++ Kayttoliittymat/app/src/main/java/com/example/pyry/kayttoliittymat/addUserNew.java
@@ -16,14 +16,15 @@ public class addUserNew extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //tata kaytetaan muokattuna ehka kohta
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //fab.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+        //        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //                .setAction("Action", null).show();
+        //    }
+        //});
     }
 
 }
