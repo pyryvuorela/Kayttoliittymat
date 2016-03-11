13624f70904ad0da631de046955bb7cee5688873
Susanna <unsbe92@gmail.com>
2016-03-11 12:20:35 +0200

    tried to implement back button
    work in progress, mostly in comments

--- Kayttoliittymat/app/src/main/java/com/example/pyry/kayttoliittymat/adminMainMenu.java
+++ Kayttoliittymat/app/src/main/java/com/example/pyry/kayttoliittymat/adminMainMenu.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.view.MenuItem;
+import android.view.Menu;
+//import android.app.ActionBar;
 
 public class adminMainMenu extends AppCompatActivity {
     Button userControl;
@@ -16,9 +20,14 @@ public class adminMainMenu extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //back button
+        //setupActionBar();
         setContentView(R.layout.activity_admin_main_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //yritettiin lisata back buttonia, ehka tarpeellisia joskus
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //getSupportActionBar().setHomeButtonEnabled(true);
 
         userControl = (Button) findViewById(R.id.usercontrolID);
         roomControl = (Button) findViewById(R.id.roomcontrolID);
@@ -35,5 +44,35 @@ public class adminMainMenu extends AppCompatActivity {
         });
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    // ehka back buttonin kaytossa oleellinen ja options menun kanssa
+    // Determines if Action bar item was selected. If true then do corresponding action.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        //handle presses on the action bar items
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+
+            case R.id.action_back:
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    //back button tyonalla
+    //private void setupActionBar() {
+    //    ActionBar actionBar = getSupportActionBar();
+    //    if (actionBar != null) {
+    //        actionBar.setDisplayHomeAsUpEnabled(true);
+    //    }
+    //}
 
 }
