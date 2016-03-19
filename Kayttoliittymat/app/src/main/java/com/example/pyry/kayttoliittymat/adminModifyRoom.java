9888bcf0efe74edad0f3efacfb12d7a16adffa06
pyryvuorela <pyyrvu@utu.fi>
2016-03-19 18:42:35 +0200

    Housemodify ei toimi vielä

--- Kayttoliittymat/app/src/main/java/com/example/pyry/kayttoliittymat/adminModifyRoom.java
+++ Kayttoliittymat/app/src/main/java/com/example/pyry/kayttoliittymat/adminModifyRoom.java
@@ -1,13 +1,21 @@
 package com.example.pyry.kayttoliittymat;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class adminModifyRoom extends AppCompatActivity {
+    HouseDatabase houseDatabase;
+    Button modifyHouse;
+    EditText selectHouse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +24,21 @@ public class adminModifyRoom extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
+        modifyHouse = (Button) findViewById(R.id.modifyHouseSelectButtonID);
+
+        modifyHouse.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Cursor res = houseDatabase.getAllData();
+                boolean found = false;
+                while(res.moveToNext()){
+                    if(selectHouse.getText().toString().equals(res.getString(0))){
+                        startActivity(new Intent(getApplicationContext(), adminHouseControl.class));
+                        found = true;
+                    }
+                }
+                if(found == false)
+                Toast.makeText(adminModifyRoom.this, "House not found", Toast.LENGTH_LONG).show();
             }
         });
     }
-
 }
